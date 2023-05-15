0c10c4aabdc207f14d0b4eca8cff49bbe91c4ed6
nauvalachid <nauvalachid@gmail.com>
2023-05-15 11:37:47 +0700

    menampilkan mahasiswa printA11

--- STATICMEMBER_117/STATICMEMBER_117.cpp
+++ STATICMEMBER_117/STATICMEMBER_117.cpp
@@ -18,3 +18,9 @@ int mahasiswa::nim = 0;
 void mahasiswa::setID() {
 	id = ++nim;
 }
+
+void mahasiswa::printA11() {
+	cout << "ID   = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+}
