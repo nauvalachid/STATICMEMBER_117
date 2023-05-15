b10ec210015b67a9b81a4c16edc071a08224437e
nauvalachid <nauvalachid@gmail.com>
2023-05-15 11:26:38 +0700

    membuat class mahasiswa

---
+++ STATICMEMBER_117/STATICMEMBER_117.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+public:
+	static int nim;
+	int id;
+	string nama;
+
+	void setID();
+	void printA11();
+	mahasiswa(string pnama) :nama(pnama) { setID(); }
+};
