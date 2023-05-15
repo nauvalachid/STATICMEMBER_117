de868f1cbd011eb245d5c1db437893a2b79a8913
nauvalachid <nauvalachid@gmail.com>
2023-05-15 18:35:46 +0700

    integer main,menampilkan hasil

--- chainfunction/chainfunction.cpp
+++ chainfunction/chainfunction.cpp
@@ -14,3 +14,13 @@ public:
 	{
 		return this->judul;
 	}
+
+} bukunya;
+
+int main()
+{
+	// bukunya.setJudul("Matematika");
+	// cout << bukunya.getJudul();
+	cout << bukunya.setJudul("Matematika").getJudul(); // chain function calls
+	return 0;
+}
